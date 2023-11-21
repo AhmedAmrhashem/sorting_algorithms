1781d976c03aba66c8492d165ca9326eb274ce73
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-11-21 12:02:44 -0800

    finalise

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,21 +9,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int min = 0;
-	int temp;
-	int i, j;
-	int Tall = size;
-	for (i = 0; i < Tall - 1 ; i++)
+	size_t i, j;
+	int *tmp = NULL, z;
+
+	for (i = 0; array && i < size - 1; i++)
 	{
-		min = i;
-		for (j = i + 1; j < Tall; j++)
+		tmp = NULL;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[min] < array[j])
-				min = j;
+			if (array[j] < array[i])
+			{
+				if (tmp && array[j] < *tmp)
+					tmp = &array[j];
+				if (!tmp)
+					tmp = &array[j];
+			}
 		}
-		temp = array[i];
-		array[i] = array[min];
-		array[min] = temp;
-		print_array(array, size);
+		if (tmp)
+		{
+			z = *tmp;
+			*tmp = array[i];
+			array[i] = z;
+			print_array(array, size);
+		}
 	}
 }
