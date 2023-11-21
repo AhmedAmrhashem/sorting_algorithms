b9559d29155f666c61bcc6d8ed950cf818bb8a68
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-11-20 21:44:42 -0800

    KKK

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,19 +5,19 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	int i;
 	int j;
+	int length = size;
 
-	for(i = 0; i < size - 1; i++)
+	for(i = 0; i < length - 1; i++)
 	{
-		for(j = 0; j < size - i - 1; j++)
+		for(j = 0; j < length - i - 1; j++)
 		{
 			if(array[j] > array[j+1])
 			{
 				temp = array[j];
 				array[j] = array[j+1];
 				array[j+1] = temp;
-				print_array(array);
+				print_array(array, size);
 			}
 		}
 	}
-	print_array(array);
 }
--- sort.h
+++ sort.h
@@ -9,6 +9,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void switch_two(int a, int b)
+void print_array(const int *array, size_t size);
 
 #endif
