e2c6880320a483e3b9fdc7861aa47b59de3724ce
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-11-21 19:24:12 -0800

    hi

--- sort.h
+++ sort.h
@@ -24,8 +24,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
-void quick_sort_recursive(int *array, int start, int end, size_t size);
-void swap(int *a, int *b);
+void divide(int st, int pi, int *i, size_t size);
+void swap_int(int *a, int *b);
 
 
 #endif
